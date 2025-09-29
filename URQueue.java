a3bf190c42f6e905def0a9fbe66f72a0c6bbd2c0
NormanSackett <133908393+NormanSackett@users.noreply.github.com>
2025-09-29 14:35:40 -0400

    Update URQueue.java

--- URQueue.java
+++ URQueue.java
@@ -1,4 +1,15 @@
 
 public class URQueue<E> extends URLinkedList<E> {
-
+	
+	public URQueue() {
+		super();
+	}
+	
+	public void enqueue(E e) {
+		this.addLast(e);
+	}
+	
+	public E dequeue() {
+		return this.pollLast().element();
+	}
 }
