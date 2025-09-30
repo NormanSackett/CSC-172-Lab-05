d0fc98f5bc3ead745fdc5dbc3de176bd617e4326
NormanSackett <133908393+NormanSackett@users.noreply.github.com>
2025-09-29 22:38:03 -0400

    Update InfixCalc.java
    added infix to postfix functionality

--- InfixCalc.java
+++ InfixCalc.java
@@ -5,11 +5,12 @@ import java.awt.event.*;
 
 public class InfixCalc {
 
-	public static void main(String[] args) { //sets up GUI and adds action to the button
+	public static void main(String[] args) { //sets up Java Swing GUI elements and adds button action event
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
 		frame.setBounds(300, 100, 300, 300);
 		frame.setLayout(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JLabel ifixInputLabel = new JLabel();
 		JTextField ifix = new JTextField();
@@ -53,6 +54,22 @@ public class InfixCalc {
 		});
 	}
 	
+	public static double evaluateostfix(URQueue<Character> queue) {
+		double output = 0;
+		URStack<Double> stack = new URStack<Double>();
+		
+		while (!queue.isEmpty()) {
+			Character c = queue.peek();
+			if (c >= '0' && c <= '9') stack.push(Double.valueOf(queue.dequeue()));
+			else if (c == '!') { //checks if the operator only takes one input
+				Double tempDoub = stack.pop();
+				if (tempDoub == 1 || tempDoub == 0) tempDoub = (double) ~tempDoub.intValue(); //bit flips for not operator
+			}
+		}
+		
+		return output;
+	}
+	
 	public static URQueue<Character> infixToPostfix(char[] input) { //performs infix-to-postfix algorithm
 		URQueue<Character> outputQueue = new URQueue<Character>();
 		URStack<Character> stack = new URStack<Character>();
@@ -61,24 +78,29 @@ public class InfixCalc {
 			Character c = input[i];
 			
 			//check is c is an operator or operand first
-			if (c >= '0' && c <= '9') outputQueue.enqueue(c); // if operand, add to the queue
-			else if (c == '(') stack.push(c); // if an open parenthesis, push to the stack
-			else if (c == ')') { // if a closed parenthesis, pop from the stack until the closed parenthesis is hit
-				while (stack.size() > 0 && stack.peek() != '(') outputQueue.enqueue(c);
+			if (c >= '0' && c <= '9') outputQueue.enqueue(c); //if operand, add to the queue
+			else if (c == '(') stack.push(c); //if an open parenthesis, push to the stack
+			else if (c == ')') { //if a closed parenthesis, pop from the stack until the open parenthesis is hit
+				while (!stack.isEmpty() && stack.peek() != '(') outputQueue.enqueue(c);
 				
 				stack.pop();
 			}
-			else if (c != ' ') { // if an operator, push to the stack in the correct priority location (spaces are ignored)
+			else if (c != ' ') { // if an operator, enqueue operators until an operator of less priority is hit (spaces are ignored)
+				while (getPriority(c) < getPriority(stack.peek())) {
+					outputQueue.enqueue(stack.pop());
+				}
+				outputQueue.enqueue(stack.pop());
 				stack.push(c);
 			}
 		}
+		while (!stack.isEmpty()) outputQueue.enqueue(stack.pop());
 
 		return outputQueue;
 	}
 	
 	public static int getPriority(Character c) {
 		//priority form https://en.wikipedia.org/wiki/Order_of_operations#Programming_languages
-		// highest priority gets the smallest return value
+		// highest priority gets the smallest return value to allow easy expansion to further operators
 		if (c == '^') return 1;
 		if (c == '!') return 2;
 		if (c == '/' || c == '*' || c == '%') return 3;
@@ -87,6 +109,6 @@ public class InfixCalc {
 		if (c == '=') return 6;
 		if (c == '&') return 7;
 		if (c == '|') return 8;
-		else return Integer.MAX_VALUE; // throw lowest possible priority if input is not an operator/ an unknown operator
+		else return Integer.MAX_VALUE; //throw lowest possible priority if input is not an operator/ an unknown operator
 	}
 }
