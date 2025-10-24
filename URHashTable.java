5aab7376e2972a3a7802fa44afadbd073866f7a9
NormanSackett <133908393+NormanSackett@users.noreply.github.com>
2025-10-24 16:54:51 -0400

    Updated URHashTable
    Added a toString method and improved null checks.

--- URHashTable.java
+++ URHashTable.java
@@ -18,12 +18,23 @@ public class URHashTable<K, V> extends UR_HashTable<K, V> implements Iterable<K>
 		values = (V[]) new Object[INIT_CAPACITY];
 		m = INIT_CAPACITY;
 	}
+	
+	public String toString() {
+		String str = "";
+		for (int i = 0; i < m; i++) {
+			str += "[" + keys[i] + ", " + values[i] + "]\n";
+		}
+		return str;
+	}
 
 	@Override
 	public void put(K key, V val) { //linear probing of hash table
+		if (key == null) return;
+		
 		int iteration = 0, index = key.hashCode() % m;
+		if (n == m) resize(m);
 		while (iteration < m) {
-			if (values[index].equals(null)) {
+			if (values[index] == null) {
 				values[index] = val;
 				keys[index] = key;
 				n++;
@@ -33,11 +44,14 @@ public class URHashTable<K, V> extends UR_HashTable<K, V> implements Iterable<K>
 			index++;
 			iteration++;
 			collisions++;
+			if (index == m) index = 0;
 		}
 	}
 
 	@Override
 	public V get(K key) {
+		if (key == null) return null;
+		
 		for (int i = 0; i < m; i++)
 			if (keys[i].equals(key))
 				return values[i];
@@ -47,6 +61,8 @@ public class URHashTable<K, V> extends UR_HashTable<K, V> implements Iterable<K>
 
 	@Override
 	public void delete(K key) {
+		if (key == null) return;
+		
 		for (int i = 0; i < m; i++)
 			if (keys[i].equals(key)) {
 				keys[i] = null;
@@ -63,7 +79,7 @@ public class URHashTable<K, V> extends UR_HashTable<K, V> implements Iterable<K>
 	@Override
 	public boolean isEmpty() {
 		for (int i = 0; i < m; i++)
-			if (!values[i].equals(null))
+			if (values[i] != null)
 				return false;
 		
 		return true;
@@ -71,13 +87,35 @@ public class URHashTable<K, V> extends UR_HashTable<K, V> implements Iterable<K>
 
 	@Override
 	public boolean contains(K key) {
+		if (key == null) return false;
+		
 		for (int i = 0; i < keys.length; i++)
-			if (keys[i].equals(key))
+			if (keys[i] != null && keys[i].equals(key))
 				return true;
 	
 		return false;
 	}
-
+	
+	@SuppressWarnings("unchecked")
+	private void resize(int capacity) {
+		if (capacity < 0) return;
+		
+		K[] tempKeys = keys.clone();
+		V[] tempVals = values.clone();
+		m = capacity * 2;
+		keys = (K[]) new Object[m];
+		values = (V[]) new Object[m];
+		n = 0;
+		inserts = 0;
+		collisions = 0;
+		
+		for (int i = 0; i < capacity; i++) {
+			if (tempKeys[i] != null) {
+				put(tempKeys[i], tempVals[i]);
+			}
+		}
+	}
+	
 	@Override
 	public Iterable<K> keys() {
 		return this;
@@ -93,7 +131,7 @@ public class URHashTable<K, V> extends UR_HashTable<K, V> implements Iterable<K>
 
 		@Override
 		public boolean hasNext() {
-			if (probeNext().equals(null))
+			if (probeNext() == null)
 				return false;
 			else
 				return true;
@@ -102,7 +140,7 @@ public class URHashTable<K, V> extends UR_HashTable<K, V> implements Iterable<K>
 		private K probeNext() {
 			int i = iteratorIndex;
 			while (i < m) {
-				if (!keys[i].equals(null))
+				if (keys[i] != null)
 					return keys[i++];
 				else i++;
 			}
@@ -112,7 +150,7 @@ public class URHashTable<K, V> extends UR_HashTable<K, V> implements Iterable<K>
 		@Override
 		public K next() {
 			while (iteratorIndex < m) {
-				if (!keys[iteratorIndex].equals(null))
+				if (keys[iteratorIndex] != null)
 					return keys[iteratorIndex++];
 				else iteratorIndex++;
 			}
